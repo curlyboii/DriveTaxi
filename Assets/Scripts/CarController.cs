a41ff1867d8d4bb95fe1fe06dea9d7f8467b8cff
Vitalii <vetalsofyk@gmail.com>
2023-04-07 17:55:57 +0300

    Changing Direction
    When you click with the mouse (but also by clicking on the screen too), the car changes direction

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public float moveSpeed;
+    bool moveLeft = true; // currently moving to the left
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,42 @@ public class CarController : MonoBehaviour
     void Update()
     {
         Move();
+        CheckInput();
     }
 
     void Move()
     {
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
+
+
+    void CheckInput() //when the player touching
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            ChangeDirection();
+
+        }
+
+    }
+
+
+    void ChangeDirection()
+    {
+        if(moveLeft)
+        {
+            moveLeft = false;
+            transform.rotation = Quaternion.Euler(0,90,0);
+
+        }
+        else
+        {
+            moveLeft = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+
+
 }
