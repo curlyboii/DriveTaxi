83231579bef7d54328235f1f5754a3fb168e76b4
Vitalii <vetalsofyk@gmail.com>
2023-04-10 14:29:33 +0300

    Reload level
    Now when the player falls, the level is reloaded

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -50,6 +51,11 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         spawnerPlatforms.SetActive(false);
+        Invoke("ReloadScene", 1f);
+    }
 
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene("Scene_0");
     }
 }
